0df4d515bedc3be317128b153bea4afaf1ce721e
ZhukovaD <zhukovaphoto.spb@gmail.com>
2018-02-05 11:16:00 +0300

    Исправляет замечания наставника

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,6 @@
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var setupSimilarList = document.querySelector('.setup-similar-list');
@@ -12,22 +11,22 @@ var wizardName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кр
 var wizardSurname = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var wizardCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var wizardEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizards = [];
+
+var getRandomElement = function (arrayName) {
+  return Math.floor(Math.random() * arrayName.length);
+};
 
 for (var i = 0; i < 4; i++) {
-  wizards.push({
-    name: wizardName[Math.floor(Math.random() * wizardName.length)] + ' ' + wizardSurname[Math.floor(Math.random() * wizardSurname.length)],
-    coatColor: wizardCoatColor[Math.floor(Math.random() * wizardCoatColor.length)],
-    eyesColor: wizardEyesColor[Math.floor(Math.random() * wizardEyesColor.length)]
-  });
-
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizardName[getRandomElement(wizardName)] + ' ' + wizardSurname[getRandomElement(wizardSurname)];
+  wizardElement.querySelector('.wizard-coat').style.fill = wizardCoatColor[getRandomElement(wizardCoatColor)];
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizardEyesColor[getRandomElement(wizardEyesColor)];
 
   fragment.appendChild(wizardElement);
 }
 setupSimilarList.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
