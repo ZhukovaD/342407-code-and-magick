6f8ba193ef0b7c753e0db693c26c727f52fe9ec5
ZhukovaD <zhukovaphoto.spb@gmail.com>
2018-02-06 13:34:32 +0300

    Переменные -> константы

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const userDialog = document.querySelector('.setup');
+var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-const similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-const setupSimilarList = document.querySelector('.setup-similar-list');
-const fragment = document.createDocumentFragment();
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var setupSimilarList = document.querySelector('.setup-similar-list');
+var fragment = document.createDocumentFragment();
 
-const WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
